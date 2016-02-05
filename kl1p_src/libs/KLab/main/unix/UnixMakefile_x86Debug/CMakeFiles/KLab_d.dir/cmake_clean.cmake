15ac4ec7005699146f454b7ee44eceac7b22b94a
Zuo Xiang <xianglinks@gmail.com>
2016-02-05 11:41:04 +0000

    add results OMP on odroid

---
+++ kl1p_src/libs/KLab/main/unix/UnixMakefile_x86Debug/CMakeFiles/KLab_d.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KLab_d.dir/home/odroid/src/kl1p_dev/kl1p_src/libs/KLab/src/DateTime.cpp.o"
+  "CMakeFiles/KLab_d.dir/home/odroid/src/kl1p_dev/kl1p_src/libs/KLab/src/Exception.cpp.o"
+  "CMakeFiles/KLab_d.dir/home/odroid/src/kl1p_dev/kl1p_src/libs/KLab/src/StringUtil.cpp.o"
+  "CMakeFiles/KLab_d.dir/home/odroid/src/kl1p_dev/kl1p_src/libs/KLab/src/Random.cpp.o"
+  "CMakeFiles/KLab_d.dir/home/odroid/src/kl1p_dev/kl1p_src/libs/KLab/src/BitArray.cpp.o"
+  "CMakeFiles/KLab_d.dir/home/odroid/src/kl1p_dev/kl1p_src/libs/KLab/src/TypeUtil.cpp.o"
+  "CMakeFiles/KLab_d.dir/home/odroid/src/kl1p_dev/kl1p_src/libs/KLab/src/Timer.cpp.o"
+  "/home/odroid/src/kl1p_dev/kl1p_src/bin/unix/libKLab_d.pdb"
+  "/home/odroid/src/kl1p_dev/kl1p_src/bin/unix/libKLab_d.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KLab_d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
